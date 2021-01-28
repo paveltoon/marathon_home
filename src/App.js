d29399e7e6d4b031e644129f4b9f977b8f57dd9c
Копылов Павел Леонидович <kopylovpl@mosreg.ru>
2021-01-28 21:12:54 +0300

    2 lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Layout from './components/Layout/Layout';
 
-function App() {
+import bg3 from './assets/bg3.jpg'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header title="Pokemon styled page" descr="Try to catch me, if you can"/>
+      <Layout urlBg={bg3}/>
+      <Layout colorBg="#555"/>
+      <Layout urlBg={bg3}/>
+      <Footer />
+    </>
+  )
 }
 
 export default App;
