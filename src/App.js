a6d2eef3e8d258e3c10918fe7685e6512d24b4f0
Копылов Павел Леонидович <kopylovpl@mosreg.ru>
2021-01-29 23:37:34 +0300

    [fix] Исправлены замечания

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ const App = () => {
   return (
     <>
       <Header title="Pokemon styled page" descr="Try to catch me, if you can"/>
-      <Layout urlBg={bg3}/>
-      <Layout colorBg="#555"/>
-      <Layout urlBg={bg3}/>
+      <Layout urlBg={bg3} id={1} title="I'm a pokemon and i'm duck."/>
+      <Layout colorBg="#555" id={2} title="I can't fly, I can just quack."/>
+      <Layout urlBg={bg3} id={3} title="And my name is dont give a fu..."/>
       <Footer />
     </>
   )
